927f8b40fa0871b5aaa6bcbdf8fb3f7df7112828
pablofiamingo <pablomatiasfiamingo@gmail.com>
2022-04-21 17:29:55 -0300

    Se agrega el proyecto al repo

---
+++ src/main/java/com/proyectoIuris/iuris/IurisApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoIuris.iuris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IurisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IurisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoIuris/iuris/IurisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoIuris.iuris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IurisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
