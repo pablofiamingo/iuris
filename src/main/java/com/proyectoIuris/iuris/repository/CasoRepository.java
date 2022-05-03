19adf82cc47ac45d52cb1126e4d2597a0e2acb5d
nadialucila <nadia.bergara17@gmail.com>
2022-05-02 21:51:36 -0300

    anda todo

--- src/main/java/com/proyectoIuris/iuris/repository/CasoRepository.java
+++ src/main/java/com/proyectoIuris/iuris/repository/CasoRepository.java
@@ -1,10 +1,18 @@
 package com.proyectoIuris.iuris.repository;
 
 import com.proyectoIuris.iuris.model.Caso;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CasoRepository extends CrudRepository<Caso, Integer> {
+    @Query("SELECT c FROM Caso c WHERE c.cliente.usuario.idUsuario = :idUsuario")
+    public List<Caso> findCasoByIdUsuario(@Param("idUsuario") int idUsuario);
+    void deleteByIdCaso(int idCaso);
+    Caso findByIdCaso(int idCaso);
+
 }
