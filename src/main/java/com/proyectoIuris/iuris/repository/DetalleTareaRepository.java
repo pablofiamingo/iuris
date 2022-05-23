ea2715b9bb8428b7af8b92acd0fe1cf6badefc38
matiasVarela076 <matiasvarela076@gmail.com>
2022-05-23 01:01:36 -0300

    lista de tareas/detalle andando

--- src/main/java/com/proyectoIuris/iuris/repository/DetalleTareaRepository.java
+++ src/main/java/com/proyectoIuris/iuris/repository/DetalleTareaRepository.java
@@ -1,9 +1,18 @@
 package com.proyectoIuris.iuris.repository;
 
-import com.proyectoIuris.iuris.model.Cliente;
 import com.proyectoIuris.iuris.model.DetalleTarea;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DetalleTareaRepository extends CrudRepository<DetalleTarea, Integer> {
+
+    @Query("SELECT detalles FROM DetalleTarea detalles WHERE listaDeTareas.idToDo = :idLista")
+    public List<DetalleTarea> getTareas(@Param("idLista")int idLista);
+
+    public DetalleTarea save(DetalleTarea tarea);
+
 
 }
