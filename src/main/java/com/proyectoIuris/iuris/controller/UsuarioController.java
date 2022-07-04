9231d407e93d25eb56a628b5b80356aaa633fa78
nadialucila <nadia.bergara17@gmail.com>
2022-07-04 00:55:36 -0300

    Limpiando y comentando UsuarioController

--- src/main/java/com/proyectoIuris/iuris/controller/UsuarioController.java
+++ src/main/java/com/proyectoIuris/iuris/controller/UsuarioController.java
@@ -27,50 +27,56 @@ public class UsuarioController {
     //Métodos por GET
     @GetMapping("/registro")
     public String getRegistro(Model model, HttpSession session) {
-        //chequeo que esté logueado
+        //chequeo que esté logueado, si no lo esta va a login, caso contrario obtengo el usuario
         if (!Util.isLogged(session)) return "redirect:/usuario/login";
-
         Usuario usuarioActivo = (Usuario) session.getAttribute("user");
-        if (!usuarioActivo.getRol().equals("admin")) {
-            return "redirect:/inicio";
-        }
-
+        //si el rol no es admin va al inicio
+        String redirect = usuarioActivo.getRol().equals("admin") ? "agregarUsuario" : "redirect:/error";
+        //envio un nuevo objeto usuario para que le carguen datos
         model.addAttribute("usuario", new Usuario());
-        return "agregarUsuario";
+        return redirect;
     }
 
     @GetMapping("/lista")
     public String getUsuarios(HttpSession session, Model model) {
+
         if (!Util.isLogged(session)) return "redirect:/usuario/login";
-        Usuario user = (Usuario) session.getAttribute("user");
-        if (!user.getRol().equals("admin")) {
-            return "redirect:/inicio";
-        }
+        Usuario usuarioActivo = (Usuario) session.getAttribute("user");
+        if (!usuarioActivo.getRol().equals("admin")) return "redirect:/error";
+
         List<Usuario> usuarios = usuarioService.list();
         model.addAttribute("resultados", usuarios);
+
         return "resultadosUsuario";
     }
 
     @GetMapping("/login")
     String getLogin(Model model, HttpSession httpSession) {
-        Usuario user = (Usuario) httpSession.getAttribute("user");
-        if(httpSession.getAttribute("user") != null) {
-            model.addAttribute("nombre", user);
-            return "redirect:/inicio";
-        }
+        //si hay usuario activo, redirige a inicio
+        //si no hay usuario logueado, envia un objeto usuario en blanco para que
+        //le coloquen los datos de inicio de sesion
+        Usuario usuarioActivo = (Usuario) httpSession.getAttribute("user");
+        if(usuarioActivo != null) return "redirect:/inicio";
+
         model.addAttribute("usuario", new Usuario());
         return "login";
     }
 
-    //Métodos por POST
     @PostMapping("/registro")
-    public String insertUser(@Validated Usuario usuario, Model model, HttpSession session) {
+    public String insertUser(@Validated Usuario usuario, Model model) {
         if(usuario != null) {
             if(usuario.getRol().equals("abogado") || usuario.getRol().equals("empleado")) {
+
                 if(Util.containsIllegals(usuario.getUser()) || Util.containsIllegals(usuario.getFullName())) {
+                    //Si el usuario a registrar no contiene datos validos
+                    //se detiene la ejecucion del registro y se notifica el error
+                    //al usuario
                     model.addAttribute("error", "true");
                     return "agregarUsuario";
                 }
+                /* si esta todo en orden, se crea el usuario
+                *     y al mismo se le asigna una lista de tareas y un calendario
+                *       para que pueda utilizar correctamente la aplicacion */
                 Usuario usuarioInsertado = usuarioService.insert(usuario);
 
                 ListaDeTareas listaDeTareas = new ListaDeTareas();
@@ -83,6 +89,8 @@ public class UsuarioController {
                 usuarioInsertado.setCalendario(calendario);
                 calendarioService.save(calendario);
 
+                /* finalmente se guardan tanto el calendario como la lista de tareas
+                en el usuario y se actualiza la bbdd */
                 usuarioService.insert(usuarioInsertado);
 
                 model.addAttribute("exito","true");
@@ -94,32 +102,41 @@ public class UsuarioController {
     }
 
     @GetMapping("/editar/{idUsuario}")
-    public String getEditarUsuario(HttpSession session, Model model, @PathVariable("idUsuario")int idUsuario) {
+    public String getEditarUsuario(HttpSession session, Model model, @PathVariable("idUsuario")int idUsuarioAEditar) {
+        /*Busca el usuario a editar por su id, obtiene el objeto
+        * y lo envia al template para la manipulacion de sus datos */
         if (!Util.isLogged(session)) return "redirect:/usuario/login";
-        Usuario user = (Usuario) session.getAttribute("user");
-        if (!user.getRol().equals("admin")) {
-            return "redirect:/inicio";
-        }
-        Usuario usuario = usuarioService.findByIdUsuario(idUsuario);
-        model.addAttribute("usuario", usuario);
+        Usuario usuarioActivo = (Usuario) session.getAttribute("user");
+        if (!usuarioActivo.getRol().equals("admin")) return "redirect:/inicio";
+
+        Usuario usuarioAEditar = usuarioService.findByIdUsuario(idUsuarioAEditar);
+        model.addAttribute("usuario", usuarioAEditar);
         return "editarUsuario";
     }
 
 
     @PostMapping("/editar")
     public String editarUsuario(HttpSession session, Model model, @Validated Usuario usuario) {
-        if (!Util.isLogged(session)) return "redirect:/usuario/login";
-        Usuario user = (Usuario) session.getAttribute("user");
-        if (!user.getRol().equals("admin")) {
-            return "redirect:/inicio";
-        }
-        if(usuario!=null) {
-            usuarioService.insert(usuario);
-            List<Caso> casos = casoService.listPermisoAbogado(usuario.getIdUsuario());
 
-            for (Caso caso : casos) {
-                caso.setRepresentante(usuario.getFullName());
-                casoService.save(caso);
+        if (!Util.isLogged(session)) return "redirect:/usuario/login";
+        Usuario usuarioActivo = (Usuario) session.getAttribute("user");
+        if (!usuarioActivo.getRol().equals("admin")) return "redirect:/inicio";
+
+        if(usuario!=null) {
+            /*
+            * Al editar el usuario, en caso de que se haya cambiado
+            * el nombre del mismo, este se actualiza en los casos
+            * que posea el usuario, si el mismo es abogado. */
+            usuarioService.insert(usuario);
+
+            if(usuario.getRol().equals("abogado")) {
+                List<Caso> casos = casoService.listPermisoAbogado(usuario.getIdUsuario());
+                if(!casos.isEmpty()) {
+                    for (Caso caso : casos) {
+                        caso.setRepresentante(usuario.getFullName());
+                        casoService.save(caso);
+                    }
+                }
             }
 
             model.addAttribute("exito", true);
@@ -131,6 +148,8 @@ public class UsuarioController {
 
     @PostMapping("/login")
     public String login(@Validated Usuario u, Model model, HttpSession httpSession) {
+        /*Para el login, se busca que exista el usuario buscandolo
+        * por su nombre de usuario, luego se comparan las contraseñas*/
         Usuario usuario = usuarioService.findByUsername(u.getUser());
 
         if(usuario != null) {
@@ -148,6 +167,7 @@ public class UsuarioController {
 
     @PostMapping("/logout")
     public String logout(HttpSession session) {
+        //elimina al usuario de la sesion actual
         session.removeAttribute("user");
         return "redirect:/usuario/login";
     }
